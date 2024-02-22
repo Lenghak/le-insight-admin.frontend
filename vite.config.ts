245d893eacbc9aeeabc808ed125903a816a339eb
lenghak <125646707+Lenghak@users.noreply.github.com>
2024-02-22 12:26:32 +0700

    Added vanilla extract

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-import { vitePlugin as remix } from "@vercel/remix-run-dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
